c9310048046a290a58433608064bd71c593dc4f9
Kevin Southwick <d00230016@dmail.dixie.edu>
2018-02-05 03:31:04 -0700

    20180205

--- Android_Development/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ Android_Development/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -14,16 +14,17 @@ import android.widget.Toast;
 //AppCompatActivity is a subclass of the Activity class that provides compatibility support for older versions of android
 public class QuizActivity extends AppCompatActivity {
 
-    private static final String TAG = "QuizActivity"; //used to identify class for debugging
+    private static final String TAG = "QuizActivity"; //used to identify the current class for debugging
     private static final String KEY_INDEX = "index"; //a constant that will be the key for the key-value pair that will be stored in the bundle.
 
-    private static final int REQUEST_CODE_CHEAT = 0; //a user-defined integer that is sent to the child activity inside startActivityForResult and then received back by the parent. It is used when an activity starts more than one type of child activity and needs to know who is reporting back.
+    private static final int REQUEST_CODE_CHEAT = 0; //a user-defined integer that is sent to the child activity inside
+    //startActivityForResult and then received back by the parent. It is used when an activity starts more than one type of child activity and needs to know who is reporting back.
+    //Use startActivityForResult to hear back from the child activity
 
-
-    private Button mTrueButton; //after the button has a resource ID, you can now access it
-    private Button mFalseButton; //after the button has a resource ID, you can now access it
-    private Button mNextButton; //after the button has a resource ID, you can now access it
-    private Button mCheatButton; //after the button has a resource ID, you can now access it
+    private Button mTrueButton; //after the button has a resource ID, you will be able to access it
+    private Button mFalseButton; //after the button has a resource ID, you will be able to access it
+    private Button mNextButton; //after the button has a resource ID, you will be able to access it
+    private Button mCheatButton; //after the button has a resource ID, you will be able to access it
     private TextView mQuestionTextView; //A user interface element that displays text to the user. Access it with the resource ID
 
     //user created Question class where mQuestionBank is the var name and it holds an array of Question objects
@@ -41,70 +42,85 @@ public class QuizActivity extends AppCompatActivity {
 
     //A Bundle is a structure that maps string keys to values of certain limited types.
     @Override //@Override means that the method is overriding the parent class
+    //the onCreate method is called after the activity instance is created but before it is put on screen
+	//an activity overrides onCreate to prepare the specifics of its UI
     protected void onCreate(Bundle savedInstanceState) { //savedInstanceState is a Bundle object containing the activity's previously saved state
         super.onCreate(savedInstanceState); //must be used to inflate the UI. Called when the activity is starting. This is where most initialization should go
         Log.d(TAG, "onCreate(Bundle) called"); //used to log a message
         setContentView(R.layout.activity_quiz); //Set the activity content from a layout resource. The resource will be inflated, adding all top-level views to the activity
 
-        if(savedInstanceState != null){ //if no data is being passed, then...
+        if(savedInstanceState != null){ //if data is being passed from a previous state, then...
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0); //getInt returns the value associated with the given key, or defaultValue if no mapping of the desired type exists for the given key.
         }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view); //associate the resource ID with the text view
 
         mTrueButton = (Button) findViewById(R.id.true_button); //associate the resource id button with the java button
+        //creates an anonymous (nameless) inner class which implements the interface's single method onClick(View)
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
+            //checkAnswer is a custom method defined below which checks the answer and returns the appropriate toast
             public void onClick(View v){
                 checkAnswer(true);
             }
         });
 
-        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton = (Button) findViewById(R.id.false_button); //associate the resource id button with the java button
+        //creates an anonymous (nameless) inner class which implements the interface's single method onClick(View)
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
+            //checkAnswer is a custom method defined below which checks the answer and returns the appropriate toast
             public void onClick(View v) {
                 checkAnswer(false);
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (Button) findViewById(R.id.next_button); //associate the resource id button with the java button
+        //creates an anonymous (nameless) inner class which implements the interface's single method onClick(View)
         mNextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                mIsCheater = false;
-                updateQuestion();
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;//increases mCurrentIndex to represent the next question in the bank
+                mIsCheater = false; //resets mIsCheater to false
+                updateQuestion(); //custom method defined below which fetches the next question from the bank
             }
         });
 
-        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton = (Button) findViewById(R.id.cheat_button); //associates the resource id button with the java button
+        //creates an anonymous (nameless) inner class which implements the interface's single method onClick(View)
         mCheatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 //start CheatActivity
                 boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
-                startActivityForResult(intent, REQUEST_CODE_CHEAT); //When you want to hear back from the child activity, you call this fActivity method
+                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);//creates the intent object which defines which activity to start
+                startActivityForResult(intent, REQUEST_CODE_CHEAT); //starts the activity
+                // When you want to hear back from the child activity, you call the above Activity method
             }
         });
 
-        updateQuestion();
+        updateQuestion(); //custom method defined below which fetches the next question from the bank
     }
 
     @Override
+    //this method is called when the user presses the back button from CheatActivity.
+    //The request code is from startActivityForResult from above
+    //The resultCode is one of two predefined constants: RESULT_OK or RESULT_CANCELLED. Setting
+    //result codes is useful when the parent needs to take different action depending on how the
+    //child activity finished.
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(resultCode != Activity.RESULT_OK){
             return;
         }
-        if(requestCode == REQUEST_CODE_CHEAT){
+        if(requestCode == REQUEST_CODE_CHEAT){ //if the back button was hit from the CheatActivity...
             if(data == null){
                 return;
             }
-            mIsCheater = CheatActivity.wasAnswerShown(data);
+            mIsCheater = CheatActivity.wasAnswerShown(data); //sets this variable depending on if the user cheated
         }
     }
 
+    //the following five methods display logigng informaiton depending on the Activity's lifecycle
     @Override
     public void onStart(){
         super.onStart();
@@ -124,10 +140,11 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     @Override
+    //saves data across a runtime configuration change, like rotation
     public void onSaveInstanceState(Bundle savedInstanceState){
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
-        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex); //this demonstrates a Bundle in action
     }
 
     @Override
@@ -142,11 +159,14 @@ public class QuizActivity extends AppCompatActivity {
         Log.d(TAG, "onDestroy() called");
     }
 
+    //sets the new question by calling the getTextResId method from the Question.java class and then
+    //using the setText method of the TextView class
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
 
+    //checks if the answer is true or not and outputs the correct toast
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
--- Android_Development/README.md
+++ Android_Development/README.md
@@ -1,3 +1,6 @@
 # CS3010
 ### Android Development
 Class was taken in Spring 2018. Some assignment instructions are given by the instructor. Other assignments were done from the book _"Android Programming: The Big Nerd Ranch Guide_".
+
+## Assignments
+* GeoQuiz - "Follow along" assignment from chapters 1-5 in the textbook. Not my original assignment
